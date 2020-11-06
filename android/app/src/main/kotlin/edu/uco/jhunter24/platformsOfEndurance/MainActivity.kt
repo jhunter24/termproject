692826e001f0f23ad8e32c5aab69a57c3ed5a333
jhunter24 <jhunter24@uco.edu>
2020-11-06 11:00:00 -0600

    initialization of project

---
+++ android/app/src/main/kotlin/edu/uco/jhunter24/platformsOfEndurance/MainActivity.kt
@@ -0,0 +1,6 @@
+package edu.uco.jhunter24.platformsOfEndurance
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flame/game/base_game.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  PlatformOfEndurance game = PlatformOfEndurance();
+  runApp(game.widget);
+}
+
+class PlatformOfEndurance extends BaseGame {
+ 
+
+
+}
+
